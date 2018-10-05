e1bef13f65a843be4aeae753104bd244017c00de
julielm77 <julielyminh@gmail.com>
2018-10-05 15:00:31 +0200

    backgound v1

---
+++ background/Background.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import './Background.css';
+import film01 from "./img/film01.png";
+import film02 from "./img/film02.png";
+import film03 from "./img/film03.png";
+import film04 from "./img/film04.png";
+import film05 from "./img/film05.png";
+import film06 from "./img/film06.png";
+import film07 from "./img/film07.png";
+import film08 from "./img/film08.png";
+import film09 from "./img/film09.png";
+import film10 from "./img/film10.jpg";
+import film11 from "./img/film11.png";
+import film12 from "./img/film12.png";
+import film13 from "./img/film13.png";
+import film14 from "./img/film14.png";
+import film15 from "./img/film15.png";
+import film16 from "./img/film16.png";
+import film17 from "./img/film17.png";
+import film18 from "./img/film18.png";
+import film19 from "./img/film19.jpg";
+import film20 from "./img/film20.jpg";
+import film21 from "./img/film21.jpg";
+import film22 from "./img/film22.jpg";
+import film23 from "./img/film23.jpg";
+import film24 from "./img/film24.png";
+import film25 from "./img/film25.jpg";
+import film26 from "./img/film26.jpg";
+import film27 from "./img/film27.jpg";
+import film28 from "./img/film28.jpg";
+import film29 from "./img/film29.jpg";
+import film30 from "./img/film30.jpg";
+import film31 from "./img/film31.jpg";
+import film32 from "./img/film32.jpg";
+import film33 from "./img/film33.jpg";
+import film34 from "./img/film34.jpg";
+import film35 from "./img/film35.jpg";
+import film36 from "./img/film36.jpg";
+
+class Background extends Component{
+    render (){
+    return (
+        <div className="affiche">
+      
+                <img src={film01} className="App-fond" alt="fond" />
+                <img src={film02} className="App-fond" alt="fond" />
+                <img src={film03} className="App-fond" alt="fond" />
+                <img src={film04} className="App-fond" alt="fond" />
+                <img src={film05} className="App-fond" alt="fond" />
+                <img src={film06} className="App-fond" alt="fond" />
+          
+
+                <img src={film07} className="App-fond" alt="fond" />
+                <img src={film08} className="App-fond" alt="fond" />
+                <img src={film09} className="App-fond" alt="fond" />
+                <img src={film10} className="App-fond" alt="fond" />
+                <img src={film11} className="App-fond" alt="fond" />
+                <img src={film12} className="App-fond" alt="fond" />
+        
+
+                <img src={film13} className="App-fond" alt="fond" />
+                <img src={film14} className="App-fond" alt="fond" />
+                <img src={film15} className="App-fond" alt="fond" />
+                <img src={film16} className="App-fond" alt="fond" />
+                <img src={film17} className="App-fond" alt="fond" />
+                <img src={film18} className="App-fond" alt="fond" />
+
+
+                <img src={film19} className="App-fond" alt="fond" />
+                <img src={film20} className="App-fond" alt="fond" />
+                <img src={film21} className="App-fond" alt="fond" />
+                <img src={film22} className="App-fond" alt="fond" />
+                <img src={film23} className="App-fond" alt="fond" />
+                <img src={film24} className="App-fond" alt="fond" />
+
+
+                <img src={film25} className="App-fond" alt="fond" />
+                <img src={film26} className="App-fond" alt="fond" />
+                <img src={film27} className="App-fond" alt="fond" />
+                <img src={film28} className="App-fond" alt="fond" />
+                <img src={film29} className="App-fond" alt="fond" />
+                <img src={film30} className="App-fond" alt="fond" />
+
+
+                <img src={film31} className="App-fond" alt="fond" />
+                <img src={film32} className="App-fond" alt="fond" />
+                <img src={film33} className="App-fond" alt="fond" />
+                <img src={film34} className="App-fond" alt="fond" />
+                <img src={film35} className="App-fond" alt="fond" />
+                <img src={film36} className="App-fond" alt="fond" />
+            
+         
+      </div>
+
+
+
+
+    )
+    }
+}
+
+export default Background;
