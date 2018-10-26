182fbc5c3e88d329397ce6c3432b81d88bd2f511
Barbara <bkpadet@gmail.com>
2018-10-26 15:20:08 +0200

    menu version 2

--- menu/src/SideBar.js
+++ menu/src/SideBar.js
@@ -3,42 +3,52 @@ import "./SideBar.css"
 import { bubble as Menu } from 'react-burger-menu'
 import salle from './icons/salle.png'
 import proch from './icons/prochainement.png'
-import cookie from './icons/cookies.svg'
+import sorti from './icons/dejasorti2.png'
 import logo from "./icons/logo.png"
+import cloche from "./icons/cloche-coul.png"
 
 
 class SideBar extends React.Component {
   showSettings(event) {
     event.preventDefault();
-
   }
 
 
-
-
-
   render() {
+
+
     return (
-      <Menu id={"sidebar"} className={"my-menu"} customBurgerIcon={ <img src={logo} /> }>
+
+      <Menu id={"sidebar"} className={"my-menu"} customBurgerIcon={<img src={logo} />}>
         <img src={logo} className="logo_cookie" alt="" />
 
-        
         <div className="enSalle">
           <img src={salle} className="cookie_enSalle" alt="" />
           <a id="home" className="menu-enSalle" href="/">En Salle</a>
+
+          <img src={cloche} className="cloche" alt="" />
+
+
+          
+             {/* <div className="borderCercle"><p>{this.props.count}</p></div> */}
+
+           
         </div>
 
         <div className="proch">
           <img src={proch} className="cookie_proch" alt="" />
           <a id="about" className="menu-proch" href="/about">Prochainement</a>
-
         </div>
+
         <div className="sorti">
-          <img src={cookie} className="cookie_sorti" alt="" />
+          <img src={sorti} className="cookie_sorti" alt="" />
           <a onClick={this.showSettings} className="menu-sorti" href="">Déja Sortis</a>
 
+          <div className="basdemenu"><h1></h1>
+          </div>
         </div>
       </Menu>
+      
     );
   }
 }
