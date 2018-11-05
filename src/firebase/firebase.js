fffdd2e1ca42d28d3c4fe51b2a41c3a4fcfa8b92
Lucie Duplat <lucie.duplat@gmail.com>
2018-11-05 11:56:32 +0100

    Firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import 'firebase/auth';
+
+
+const config = {
+    apiKey: "AIzaSyC67SuNCu0XVL4Q1TJfccHL7sS6BNzt5QY",
+    authDomain: "accountusers-3d979.firebaseapp.com",
+    databaseURL: "https://accountusers-3d979.firebaseio.com",
+    projectId: "accountusers-3d979",
+    storageBucket: "accountusers-3d979.appspot.com",
+    messagingSenderId: "110709288031",
+  };
+  
+
+  if(!firebase.apps.length) {
+      firebase.initializeApp(config);
+  }
+
+  const auth = firebase.auth();
+
+  export {
+      auth,
+ };
+
---
+++ src/firebase/index.js
@@ -0,0 +1,10 @@
+import * as auth from './auth';
+import * as firebase from './firebase';
+//import 'font-awesome/css/font-awesome.min.css';
+//import 'bootstrap-css-only/css/bootstrap.min.css'; 
+//import 'mdbreact/dist/css/mdb.css'
+
+export {
+  auth,
+  firebase,
+};
