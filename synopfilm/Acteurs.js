1413e8f2916d1bc136e4b15e0f3b35c0bf2ff3b3
Anaïs Bencherifa <bencherifa.anais@gmail.com>
2018-11-05 16:12:00 +0100

    on croise les doigts

--- synopfilm/Acteurs.js
+++ synopfilm/Acteurs.js
@@ -1,6 +1,6 @@
 import React , { Component } from 'react';
 
-//import ImageNull from './test3.png';
+import ImageNull from './test3.png';
 
 
 class Acteurs extends Component{
@@ -8,11 +8,11 @@ class Acteurs extends Component{
 
      render(){
         const image_key ="https://image.tmdb.org/t/p/w500";
-        //const imageSrc = this.props.image ? `${image_key}${this.props.photographie}` : ImageNull
+        const imageSrc = this.props.image ? `${image_key}${this.props.photographie}` : ImageNull
 
          return(
            
-            <div className="containerActor">
+            <div className="containerActeurs">
                 <img className= "actorProfile" src={`${image_key}${this.props.photographie}`} alt="photographie de l'acteur"/>
                 <p className="actorName">{this.props.acteurName}</p>
             </div>
--- synopfilm/FicheFilms.js
+++ synopfilm/FicheFilms.js
@@ -6,23 +6,25 @@ import AffichGenres from "./AffichGenres.js";
 import "./FicheFilms.css"
 
 const api_key = "f43e81e7b5860bfeb6d036dd3dd602e1";
-const movie = 360920
+const movie = 424694
+//424694
+// 360920
 
 class Fiches extends Component {
 
     state = {
-        title : undefined,
-        image : undefined,
+        title: undefined,
+        image: undefined,
         description: undefined,
         id: undefined,
         realisateur: undefined,
         temps: undefined,
-        imageFond : undefined,
+        imageFond: undefined,
         genres: [],
         acteurs: [],
-        date : undefined,
-      }
-    
+        date: undefined,
+    }
+
     // Récupération de l'api pour le titre, l'image, le synopsis, l'id, la durée, l'image de fond et les genres
     getInfo = async () => {
         const recup_data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${api_key}&language=fr-FR`)
@@ -55,9 +57,9 @@ class Fiches extends Component {
     getLaDate = async () => {
         const recup_date = await fetch(`https://api.themoviedb.org/3/movie/${this.state.id}/release_dates?api_key=${api_key}`)
         const api_date = await recup_date.json();
-        const map_data = await api_date.results.filter(element =>{
+        const map_data = await api_date.results.filter(element => {
             if (element.iso_3166_1 === "FR") {
-                return element.release_dates[0].release_date 
+                return element.release_dates[0].release_date
             }
         })
         const release = map_data[0].release_dates[0].release_date
@@ -68,44 +70,48 @@ class Fiches extends Component {
     }
 
     // On lui dit de charger d'abord, une api puis l'autre pour ne pas qu'il fasse tout en même temps 
-    componentDidMount = async () =>{
+    componentDidMount = async () => {
         await this.getInfo();
         await this.getCrew();
         this.getLaDate();
     }
 
-    render(){
+    render() {
         return (
             <div>
-                
-                    <Affichagebis
-                        imageFond={this.state.imageFond}
-                        title={this.state.title} 
-                        image={this.state.image}
-                        description={this.state.description}
-                        realisateur={this.state.realisateur}
-                        temps={this.state.temps}
-                        date={this.state.date}
-                    />
 
-                    {this.state.genres.map((elem,index)=>
-                        <AffichGenres
-                        key={index}
-                        genres={elem.name}
-                        />
-                    )}
-                    
+                <Affichagebis
+                    imageFond={this.state.imageFond}
+                    title={this.state.title}
+                    image={this.state.image}
+                    description={this.state.description}
+                    realisateur={this.state.realisateur}
+                    temps={this.state.temps}
+                    date={this.state.date}
+                />
+                <div className="containerGenreActeur">
+                    <div className="containerGenres">
+                        {this.state.genres.map((elem, index) =>
+                            <AffichGenres
+                                key={index}
+                                genres={elem.name}
+                            />
+                        )}
+                    </div>
+                    <div className="containerActors">
 
-                    {/* Je lui demande d'afficher que les 4 premiers acteurs */}
-                    {this.state.acteurs.slice(0,4).map((element,id)=>
-                        <Acteurs
-                            key={id}
-                            acteurName={element.name} 
-                            photographie={element.profile_path}/> 
-                    )}
-                    
-                    
+                        {/* Je lui demande d'afficher que les 4 premiers acteurs */}
+                        {this.state.acteurs.slice(0, 4).map((element, id) =>
+                            <Acteurs
+                                key={id}
+                                acteurName={element.name}
+                                photographie={element.profile_path} />
+                        )}
+                    </div>
+                </div>
+
             </div>
-    )}
+        )
+    }
 }
 export default Fiches;
