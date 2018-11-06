3dca31b437bb5bb18ffeeac328394d02f6b090e2
veensy <veensy@gmail.com>
2018-11-06 23:55:24 +0100

    page avoir

--- src/components/profil/AffichageAvoir.js
+++ src/components/profil/AffichageAvoir.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
+import ImageNull from '../slides/imagesSlides/afficheNull.png' 
 
 import "./Avoir.css";
 
@@ -6,11 +7,17 @@ class AffichageAvoir extends Component{
     
 
     render(){
+        const image_key = "https://image.tmdb.org/t/p/w500";
+        const imageSrc = this.props.image ? `${image_key}${this.props.image}` : ImageNull
     return(
         
-        <div className="" >
-            <p>{this.props.title}</p>
-            
+        <div className="wrap" >
+            <div className="wrappeur">
+                <h2>{this.props.title}</h2>
+                <img className="" src={imageSrc} alt="" />
+                <p>{this.props.description}</p>
+                <h3>{this.props.release}</h3>
+            </div>
         </div>
     )
     }
